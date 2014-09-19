6bdfddb623d2987614701d5e4e1cfa80e6281db8
Bryan Lee <Bryan.norman.lee@gmail.com>
2014-09-19 01:14:39 -0700

    Added c_str(), removed operator<<

--- LP2PM_Exception.h
+++ LP2PM_Exception.h
@@ -7,13 +7,14 @@ private:
 public:
 	LP2PM_Exception(int type, const char* msg):
 		error_type(type),error_message(msg){};
-	LP2PM_Exception(int type):error_type(type),error_message("");
+	LP2PM_Exception(int type):error_type(type),error_message(""){}
 	~LP2PM_Exception(){};
 	
 	int type()				const { return error_type; }
 	std::string message()	const { return error_message; }
 	std::string msg()		const { return error_message; }
+	const char*	c_str()		const { return error_message.c_str(); }
 	
-	friend ostream& operator<< (ostream &out, LP2PM_Exception &e)
-	{	return out << error_message; }
-}
+	//friend std::ostream& operator<< (std::ostream &out, LP2PM_Exception &e)
+	//{	return (out << error_message); }
+};
